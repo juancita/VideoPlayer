2d042211be9d7b4302994db693c7613f3e84951c
unknown <nito9911@gmail.com>
2025-02-24 10:57:38 -0500

    Final versión del proyecto

--- video-player-backend/src/models/user.js
+++ video-player-backend/src/models/user.js
@@ -1,11 +1,17 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/database");
 
-const User = sequelize.define("User", {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  username: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.STRING, defaultValue: "admin" }, // admin o usuario normal
-});
+const User = sequelize.define(
+  "User",
+  {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, defaultValue: "admin" }, // admin o usuario normal
+  },
+  {
+    timestamps: false, // 🚨 Esto evita que Sequelize espere `createdAt` y `updatedAt`
+  }
+);
 
 module.exports = User;
