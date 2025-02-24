e0ae21f284895d193c64bdf7d3d532d89bfd37db
unknown <nito9911@gmail.com>
2025-02-24 12:55:35 -0500

    Final versión del proyecto

--- video-player-backend/src/config/database.js
+++ video-player-backend/src/config/database.js
@@ -7,28 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,  // Asegurar que se está usando el puerto
     dialect: "postgres",
     logging: false, // Desactiva logs SQL
   }
 );
 
-// ✅ Intentar la conexión varias veces antes de fallar
-const connectWithRetry = async (retries = 5, delay = 5000) => {
-  for (let i = 0; i < retries; i++) {
-    try {
-      await sequelize.authenticate();
-      console.log("✅ Conexión a PostgreSQL establecida");
-      return;
-    } catch (error) {
-      console.error(`❌ Intento ${i + 1} de conexión fallido. Retrying in ${delay / 1000} seconds...`);
-      await new Promise(res => setTimeout(res, delay));
-    }
-  }
-  console.error("❌ No se pudo conectar a PostgreSQL después de varios intentos.");
-  process.exit(1);
-};
-
-connectWithRetry(); // Llamar a la función de conexión
+// Verificar conexión
+sequelize.authenticate()
+  .then(() => console.log("✅ Conexión a PostgreSQL establecida"))
+  .catch(err => console.error("❌ Error al conectar con PostgreSQL:", err));
 
 module.exports = { sequelize };
