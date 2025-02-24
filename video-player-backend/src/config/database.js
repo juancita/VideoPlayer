016b96b10a7f4a7f31d21e3625941027538af723
unknown <nito9911@gmail.com>
2025-02-24 14:55:34 -0500

    Final versión del proyecto

--- video-player-backend/src/config/database.js
+++ video-player-backend/src/config/database.js
@@ -1,20 +1,22 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const DB_HOST = process.env.DB_HOST || "localhost"; //  Si no hay variable, usa localhost
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: DB_HOST,
     dialect: "postgres",
-    logging: false, // 
+    logging: false,
   }
 );
 
 // Verificar conexión
 sequelize.authenticate()
-  .then(() => console.log("Conexión a PostgreSQL establecida"))
+  .then(() => console.log(` Conectado a PostgreSQL en ${DB_HOST}`))
   .catch(err => console.error(" Error al conectar con PostgreSQL:", err));
 
 module.exports = { sequelize };
